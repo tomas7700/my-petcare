f00be022562016030ff34bdfd45051a471ec1387
tomas7700 <122415556+tomas7700@users.noreply.github.com>
2023-12-01 06:52:40 -0500

    Update mobile-sidebar.tsx

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-
+import Sidebar from "./sidebar";
 import {Menu} from 'lucide-react';
 import { Sheet, SheetTrigger,SheetContent } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
